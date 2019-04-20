36dbd0153e8c7519d31fa89d1bccb7a016d9fc08
Mhlangano <mhlanganok@yahoo.com>
2019-04-20 20:30:36 +0200

    MK Test

---
+++ src/.forceCode/mhlanganok@absa.com/.sfdx/typings/lwc/apex/customLookUpController.d.ts
@@ -0,0 +1,6 @@
+declare module "@salesforce/apex/customLookUpController.fetchDistinctRecord" {
+  export default function fetchDistinctRecord(param: {field_name: any, object_name: any, search_value: any}): Promise<any>;
+}
+declare module "@salesforce/apex/customLookUpController.fetchLookUpValues" {
+  export default function fetchLookUpValues(param: {searchKeyWord: any, ObjectName: any, fieldName: any}): Promise<any>;
+}
